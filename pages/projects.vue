0d51c56af8cdd6eca2c4118ef4d1a1ea837de83b
Andrea Corriga <cloud_aso@hotmail.it>
2024-02-03 11:25:15 +0100

    Added seo features

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,7 +1,43 @@
 <template>
+    <Head>
+        <Title> {{ title }}</Title>
+
+        <Link rel="sitemap" :href="requestUrl.origin + '/' + 'sitemap.xml'" type="application/xml" />
+        <Link rel="canonical" :href="requestUrl.href"/>
+
+        <Meta name="description" :content="description" />
+        <Meta name="author" :content="constants.meta.author" />	
+        <Meta name="keywords" :lang="constants.meta.language" :content="constants.meta.keywords" />
+        <Meta name="robots" :content="constants.meta.robots" />
+        <Meta name="language" :content="constants.meta.language" />
+        <Meta name="rating" :content="constants.meta.rating" />
+
+        <!--Facebook OpenGraph-->
+        <Meta property="og:url" :content="requestUrl.href" />
+        <Meta property="og:title" :content="title" />
+        <Meta property="og:image" :content="constants.facebook_open_graph.image" />
+        <Meta property="og:description" :content="description" />
+
+    </Head>
+
     <LayoutHeader />
 
     <ProjectsMain /> 
     
     <LayoutFooter />
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Projects" + constants.\second_title, 
+            description: "Andrea Corriga is a Ph.D. and Software Engineer currently living in Sardinia, Italy. Passionate about sport, music, video games and programming he can be found in all social networks or video games with the username AsoStrife.", 
+
+            nuxtApp: useNuxtApp(),
+            requestUrl: useRequestURL()
+
+        }
+    }
+}
+</script>
--- utils/constants.js
+++ utils/constants.js
@@ -1,95 +1,93 @@
 /* eslint-disable prettier/prettier */
 let constants = {
     main: {
-        "name": "Andrea",
-        "surname": "Corriga",
-        "full_name": "Andrea Corriga",
-        "username": "AsoStrife",
-        "slogan": "Ph.D. & Software Engineer",
-        "main_title": "Andrea Corriga (@AsoStrife) • Ph.D. and Software Engineer",
-        "second_title": " • Andrea Corriga (@AsoStrife)",
-        "description": "Andrea Corriga is a Ph.D. and Software Engineer currently living in Sardinia, Italy. Passionate about sport, music, video games and programming he can be found in all social networks or video games with the username AsoStrife.",
+        name: "Andrea",
+        last_name: "Corriga",
+        full_name: "Andrea Corriga",
+        username: "AsoStrife",
+        slogan: "Ph.D. & Software Engineer",
+        main_title: "Andrea Corriga (@AsoStrife) • Ph.D. and Software Engineer",
+        second_title: " • Andrea Corriga (@AsoStrife)",
+        description: "Andrea Corriga is a Ph.D. and Software Engineer currently living in Sardinia, Italy. Passionate about sport, music, video games and programming he can be found in all social networks or video games with the username AsoStrife.",
     },
     meta: {
-        "author": "Andrea Corriga (@AsoStrife)",
-        "email": "me@andreacorriga.com",
-
-        "keywords": "Andrea, Corriga, Aso, AsoStrife, Cloud, CloudAso, Microsoft, Student, Partner, MSP, Portfolio, Personal Website, Webmaster, Web, Developer, PHP, Javascript, Java, Keras, Vue, Crypto, Coin, Tensorflow, Strapi,  Python, Streamer, Twitch, University, Master, Graduate, Computer Science, Coding",
-        "robots": "index, follow",
-        "language": "en",
-        "rating": "general",
-        "cv": "https://cv.andreacorriga.com"
+        author: "Andrea Corriga (@AsoStrife)",
+        email: "me@andreacorriga.com",
+        keywords: "Andrea, Corriga, Aso, AsoStrife, Cloud, CloudAso, Microsoft, Student, Partner, MSP, Portfolio, Personal Website, Webmaster, Web, Developer, PHP, Javascript, Java, Keras, Vue, Crypto, Coin, Tensorflow, Strapi,  Python, Streamer, Twitch, University, Master, Graduate, Computer Science, Coding",
+        robots: "index, follow",
+        language: "en",
+        rating: "general",
+        cv: "https://cv.andreacorriga.com"
     },
     urls: {
-        "iris_thesis": "https://iris.unica.it/handle/11584/330467"
+        iris_thesis: "https://iris.unica.it/handle/11584/330467"
     },
     img: {
-        "profile": "/img/profile/andrea-corriga-asostrife-profile-image.png",
-        "profile_real": "/img/profile/andrea-corriga-asostrife-profile-real-image.jpg",
-        "profile_200x200": "/img/profile/andrea-corriga-asostrife-profile-image-200x200.png",
-        "profile_chocobo": "/img/profile/andrea-corriga-asostrife-chocobo-small.png",
-        "favicon": "/favicon/favicon.png",
-        "favicon_72x72": "/img/favicon/favicon-72x72.png",
-        "favicon_114x114": "/img/favicon/favicon-114x114.png"
+        profile: "/img/profile/andrea-corriga-asostrife-profile-image.png",
+        profile_real: "/img/profile/andrea-corriga-asostrife-profile-real-image.jpg",
+        profile_200x200: "/img/profile/andrea-corriga-asostrife-profile-image-200x200.png",
+        profile_chocobo: "/img/profile/andrea-corriga-asostrife-chocobo-small.png",
+        favicon: "/favicon/favicon.png",
+        favicon_72x72: "/img/favicon/favicon-72x72.png",
+        favicon_114x114: "/img/favicon/favicon-114x114.png"
     },
     social: [
         {
-            "name": "Facebook",
-            "url": "https://facebook.com/AsoStrife",
-            "icon": "fab fa-facebook"
+            name: "Facebook",
+            url: "https://facebook.com/AsoStrife",
+            icon: "fab fa-facebook"
         },
         {
-            "name": "Instagram",
-            "url": "https://instagram.com/AsoStrife",
-            "icon": "fab fa-instagram"
+            name: "Instagram",
+            url: "https://instagram.com/AsoStrife",
+            icon: "fab fa-instagram"
         },
         {
-            "name": "Twitter",
-            "url": "https://twitter.com/AsoStrife",
-            "icon": "fab fa-twitter"
+            name: "Twitter",
+            url: "https://twitter.com/AsoStrife",
+            icon: "fab fa-twitter"
         },
         {
-            "name": "Github",
-            "url": "https://github.com/AsoStrife",
-            "icon": "fab fa-github-alt"
+            name: "Github",
+            url: "https://github.com/AsoStrife",
+            icon: "fab fa-github-alt"
         },
         {
-            "name": "Linkedin",
-            "url": "https://www.linkedin.com/in/andrea-corriga-20254465/",
-            "icon": "fab fa-linkedin-in"
+            name: "Linkedin",
+            url: "https://www.linkedin.com/in/andrea-corriga-20254465/",
+            icon: "fab fa-linkedin-in"
         },
         // {
-        //     "name": "Telegram",
-        //     "url": "https://t.me/AsoStrife",
-        //     "icon": "fab fa-telegram"
+        //     name: "Telegram",
+        //     url: "https://t.me/AsoStrife",
+        //     icon: "fab fa-telegram"
         // },
         {
-            "name": "Twitch",
-            "url": "https://www.twitch.tv/asostrife",
-            "icon": "fab fa-twitch"
+            name: "Twitch",
+            url: "https://www.twitch.tv/asostrife",
+            icon: "fab fa-twitch"
         },
         {
-            "name": "Amazon",
-            "url": "https://www.amazon.it/hz/wishlist/ls/1WJMAAM7SOJQZ/ref=nav_wishlist_lists_1?_encoding=UTF8&type=wishlist",
-            "icon": "fab fa-amazon"
+            name: "Amazon",
+            url: "https://www.amazon.it/hz/wishlist/ls/1WJMAAM7SOJQZ/ref=nav_wishlist_lists_1?_encoding=UTF8&type=wishlist",
+            icon: "fab fa-amazon"
         },
         {
-            "name": "Google Scholar",
-            "url": "https://scholar.google.com/citations?user=fRRyZyoAAAAJ&hl=it",
-            "icon": "fab fa-google"
+            name: "Google Scholar",
+            url: "https://scholar.google.com/citations?user=fRRyZyoAAAAJ&hl=it",
+            icon: "fab fa-google"
         },
         {
-            "name": "Paypal",
-            "url": "https://www.paypal.com/paypalme/AsoStrife",
-            "icon": "fab fa-paypal"
+            name: "Paypal",
+            url: "https://www.paypal.com/paypalme/AsoStrife",
+            icon: "fab fa-paypal"
         }
     ],
     facebook_open_graph: {
-        "app_id": "2210608369160877",
-        "type": "profile",
-        "url": "https://andreacorriga.com/",
-        "image": "https://andreacorriga.com/img/profile/andrea-corriga-asostrife-profile-image-200x200.png",
-        "description": "Andrea Corriga is a Ph.D and Software Engineer currently living in Sardinia, Italy. Passionate about sport, music, video games and programming he can be found in all social networks or video games with the username AsoStrife."
+        app_id: "2210608369160877",
+        type: "profile",
+        url: "https://andreacorriga.com/",
+        image: "https://andreacorriga.com/img/profile/andrea-corriga-asostrife-profile-image-200x200.png",
     },
     skills: ['C', 'C++', 'C#', 'PHP', 'Java', 'Javascript', 'Node', 'Python', 'HTML5', 'CSS3', 'Codeigniter', 'Springboot', 'Framework7', 'Vue', 'jQuery', 'Vite', 'Capacitor', 'Electron', 'Bootstrap', 'Tailwind', 'Flask', 'FastAPI', 'Strapi', 'Mysql', 'MongoDB', 'Sass', 'Less', 'Machine Learning', 'Reinforcement Learning', 'Generative AI', 'Keras', 'Tensorflow', 'Scikit-learn', 'Pandas', 'Numpy','Microsoft Azure', 'Amazon AWS', 'Google Cloud Platform', 'Firebase'],
     projects: [
@@ -134,16 +132,16 @@ let constants = {
         }
     ],
     strife_coin: {
-        "name": "Strife Coin",
-        "symbol": "$ASO",
-        "icon": "/img/strife_coin/logo_32x32.png",
-        "address": "0x2515d83c1150d8fe4b71f9dac5db558c08675938",
-        "token_url": "https://bscscan.com/token/0x2515d83c1150d8fe4b71f9dac5db558c08675938",
-        "contract_url": "https://bscscan.com/address/0x2515d83c1150d8fe4b71f9dac5db558c08675938",
-        "pancakeswap": "https://pancakeswap.finance/swap?outputCurrency=0x2515d83c1150d8Fe4b71F9dAc5DB558C08675938",
-        "total_supply": 500000000,
-        "circulatin_supply": 300000000,
-        "decimals": 18,
+        name: "Strife Coin",
+        symbol: "$ASO",
+        icon: "/img/strife_coin/logo_32x32.png",
+        address: "0x2515d83c1150d8fe4b71f9dac5db558c08675938",
+        token_url: "https://bscscan.com/token/0x2515d83c1150d8fe4b71f9dac5db558c08675938",
+        contract_url: "https://bscscan.com/address/0x2515d83c1150d8fe4b71f9dac5db558c08675938",
+        pancakeswap: "https://pancakeswap.finance/swap?outputCurrency=0x2515d83c1150d8Fe4b71F9dAc5DB558C08675938",
+        total_supply: 500000000,
+        circulatin_supply: 300000000,
+        decimals: 18,
     },
     gallery: [
         {
@@ -198,20 +196,20 @@ let constants = {
     ],
     repositories: [
         {
-            "name": "BlockAPI: Blockchain analytics API",
-            "url": "https://github.com/blockchain-unica/blockapi"
+            name: "BlockAPI: Blockchain analytics API",
+            url: "https://github.com/blockchain-unica/blockapi"
         },
         {
-            "name": "Computer Vision Project",
-            "url": "https://github.com/AsoStrife/Computer-Vision-Project"
+            name: "Computer Vision Project",
+            url: "https://github.com/AsoStrife/Computer-Vision-Project"
         },
         {
-            "name": "Framework7 Facebook Login",
-            "url": "https://github.com/AsoStrife/Framework7-Facebook-Login"
+            name: "Framework7 Facebook Login",
+            url: "https://github.com/AsoStrife/Framework7-Facebook-Login"
         },
         {
-            "name": "Delaunay Triangulation",
-            "url": "https://github.com/AsoStrife/Delaunay-Triangulation"
+            name: "Delaunay Triangulation",
+            url: "https://github.com/AsoStrife/Delaunay-Triangulation"
         }
     ],
     routes: [
