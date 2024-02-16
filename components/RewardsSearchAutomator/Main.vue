bc5d89cb99c03c0db6f4ea246669231bce765de2
Andrea Corriga <cloud_aso@hotmail.it>
2024-02-16 15:07:08 +0100

    Update Main.vue

--- components/RewardsSearchAutomator/Main.vue
+++ components/RewardsSearchAutomator/Main.vue
@@ -6,20 +6,17 @@
                 <h2> Thanks for using Rewards Search Automator </h2>
             </div>
 
-            <div class="col-12 col-lg-8 col-md-8 mt-5">
-                <div v-for="project in constants.rewardsDiscoverMore" :key="project.name">
-                <ProjectsProjectCard :project="project"/>
-            </div>
-            </div>
-
             <div class="col-12 col-lg-2 col-md-2">
                 <ClientOnly>
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-slot="2175628198"
-                        data-ad-format="auto"
+                    <ins class="adsbygoogle" style="display:block" data-ad-slot="2175628198" data-ad-format="auto"
                         data-full-width-responsive="true"></ins>
                 </ClientOnly>
+            </div>
+
+            <div class="col-12 col-lg-8 col-md-8 mt-5">
+                <div v-for="project in constants.rewardsDiscoverMore" :key="project.name">
+                    <ProjectsProjectCard :project="project" />
+                </div>
             </div>
 
         </div>
@@ -28,9 +25,8 @@
 
 <style scoped>
 .page-content {
-    text-align: left  !important;
+    text-align: left !important;
 }
-
 </style>
 
 <script>
