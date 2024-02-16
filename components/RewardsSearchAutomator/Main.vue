0fdb63a9155df289043e2589f4d2200a9e021ed8
Andrea Corriga <cloud_aso@hotmail.it>
2024-02-16 15:28:19 +0100

    Fixed ads layout

--- components/RewardsSearchAutomator/Main.vue
+++ components/RewardsSearchAutomator/Main.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="container page-content">
-        <div class="row">
-
+        
+        <!-- <div class="row">
             <div class="col-12 col-lg-12 col-md-12 mt-5">
                 <h2> Thanks for using Rewards Search Automator </h2>
             </div>
+        </div> -->
 
-            <div class="col-12 col-lg-2 col-md-2">
+        <div class="row">
+            <div class="col-12 col-lg-3 col-md-3">
+                <h3 class="mb-3">ADS</h3>
                 <ClientOnly>
                     <ins class="adsbygoogle" style="display:block" data-ad-slot="2175628198" data-ad-format="auto"
                         data-full-width-responsive="true"></ins>
                 </ClientOnly>
             </div>
 
-            <div class="col-12 col-lg-8 col-md-8 mt-5">
+            <div class="col-12 col-lg-8 col-md-8">
+                <h3 class="mb-3">Discover more</h3>
                 <div v-for="project in constants.rewardsDiscoverMore" :key="project.name">
                     <ProjectsProjectCard :project="project" />
                 </div>
